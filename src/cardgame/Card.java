f37d50873026ac701e189752910e9dcfbdc7ec9f
martineng <Martin.ECA.6651@gmail.com>
2017-08-07 13:35:17 +0800

    Updated Card and Deck classes. Created CardLink class.

--- src/cardgame/Card.java
+++ src/cardgame/Card.java
@@ -17,10 +17,65 @@ public class Card
         // Null Constructor
     }
     
-    public Card(String inName, int inRank)
+    public Card(int inInput)
     {
-        setName(inName);
-        setRank(inRank);
+        switch (inInput)
+        {
+            case 1:
+                setName("1");
+                setRank(1);
+                break;
+            case 2:
+                setName("2");
+                setRank(2);
+                break;
+            case 3:
+                setName("3");
+                setRank(3);
+                break;
+            case 4:
+                setName("4");
+                setRank(4);
+                break;
+            case 5:
+                setName("5");
+                setRank(5);
+                break;
+            case 6:
+                setName("6");
+                setRank(6);
+                break;
+            case 7:
+                setName("7");
+                setRank(7);
+                break;
+            case 8:
+                setName("8");
+                setRank(8);
+                break;
+            case 9:
+                setName("9");
+                setRank(9);
+                break;
+            case 10:
+                setName("10");
+                setRank(10);
+                break;
+            case 11:
+                setName("J");
+                setRank(11);
+                break;
+            case 12:
+                setName("Q");
+                setRank(12);
+                break;
+            case 13:
+                setName("K");
+                setRank(13);
+                break;      
+            default:
+                break;
+        } // END switch
     }
     
     public void setName(String inName)
---
+++ src/cardgame/CardLink.java
@@ -0,0 +1,30 @@
+/*
+
+ */
+package cardgame;
+
+/**
+ @author martineca
+ */
+
+public class CardLink
+{
+    private Card theCard;
+    private CardLink nextLink;
+    
+    public CardLink(Card inCard)
+    {
+        theCard = inCard;
+    }
+    
+    public void setNextLink(CardLink inCardLink)
+    {
+        nextLink = inCardLink;
+    }
+    
+    public CardLink getNextLink()
+    {
+        return nextLink;
+    }
+    
+}
